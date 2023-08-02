e87ef4ab55f40c96939bdf454ad368b25b0293a0
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-08-02 16:18:48 -0300

    Solution to the challenge

--- Challenges/01-profile-card/src/index.js
+++ Challenges/01-profile-card/src/index.js
@@ -3,16 +3,54 @@ import { createRoot } from 'react-dom/client';
 import './styles.css';
 
 function App() {
+  const name = 'von Schappler';
+  const about =
+    'I am a brazilian autistic individual, passionate with coding. I am currently learning React, altough I know a bit of it already! Among other stuff, I am graduated on Computational Physics and worked for 5 years with Geophysical Data Processing in 2 different companies.';
   return (
     <div className='card'>
-      <Avatar />
+      <Avatar file='img/profile.png' />
       <div className='data'>
-        <Intro />
-        {/* Should contain one Skill component
-        for each web dev skill that you have,
-        customized with props */}
+        <Intro name={name} bio={about} />
         <SkillList />
       </div>
+    </div>
+  );
+}
+
+function Avatar(props) {
+  return <img src={props.file} alt='Profile' className='avatar' />;
+}
+
+function Intro(props) {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <p>{props.bio}</p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className='skill-list'>
+      <Skill name='HTML+CCS' emoji='😆' bg='red' />
+      <Skill name='Python' emoji='🤯' bg='lightblue' />
+      <Skill name='React' emoji='📖' bg='#16b2ab' />
+      <Skill name='Node' emoji='💥' bg='aquamarine' />
+      <Skill name='GXC' emoji='🤔' bg='green' />
+    </div>
+  );
+}
+
+function Skill(props) {
+  const style = {
+    backgroundColor: props.bg,
+  };
+  return (
+    <div>
+      <span className='skill' style={style}>
+        {props.name} {props.emoji}
+      </span>
     </div>
   );
 }
