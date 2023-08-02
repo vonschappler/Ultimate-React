a7bdbb91164a847f347be5f94c67542327d459a2
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-08-02 10:19:59 -0300

    Final code

--- Section_04/13-promises/script.js
+++ Section_04/13-promises/script.js
@@ -1,3 +1,13 @@
 // --------------------------
 // Working with promises
 // --------------------------
+
+const API = 'https://jsonplaceholder.typicode.com/todos';
+fetch(API)
+  .then((res) => res.json())
+  .then((data) => console.log(data))
+  .catch(console.log('Some error handler message or function...'));
+
+console.log(
+  'This will happen fisrt because the promise is still running on background and being processed.\n\nPromisses are asyncronous'
+);
