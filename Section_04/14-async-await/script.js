c35a99e18026dcbcf4e2679230e691e430f8fe7b
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-08-02 10:31:51 -0300

    Final code

--- Section_04/14-async-await/script.js
+++ Section_04/14-async-await/script.js
@@ -2,3 +2,23 @@
 // Working with Async/Await
 // --------------------------
 
+const API = 'https://jsonplaceholder.typicode.com/todos/1';
+
+async function getTodos() {
+  const res = await fetch(API);
+  const data = await res.json();
+  return data;
+}
+
+const todosNotAwait = getTodos();
+// This will return a pending promise
+console.log(todosNotAwait);
+console.log('This will be printed before the promise is resolved or rejected!');
+
+// This will
+const todosAwait = await getTodos();
+console.log(todosAwait);
+
+console.log(
+  'This will be printed just after the promise is resolved or rejected'
+);
