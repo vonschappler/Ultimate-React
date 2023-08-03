e52539acdc45a0869dd0e9c60344b41a706a3020
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-08-03 14:56:59 -0300

    Final code for section

--- Section_05/pizza-menu/src/index.js
+++ Section_05/pizza-menu/src/index.js
@@ -82,15 +82,13 @@ function Menu() {
 }
 
 function Pizza({ pizzaObj }) {
-  if (pizzaObj.soldOut) return null;
-
   return (
-    <li className='pizza'>
+    <li className={`pizza ${pizzaObj.soldOut && 'sold-out'}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>${pizzaObj.price}</span>
+        <span>{!pizzaObj.soldOut ? `${pizzaObj.price}` : `SOLD OUT`}</span>
       </div>
     </li>
   );
@@ -142,4 +140,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
