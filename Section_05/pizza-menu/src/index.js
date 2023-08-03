1e7d5ee2fa547e3cfedaf4b15f449ed50c4a0bc0
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-08-03 13:22:14 -0300

    Final code for lesson

--- Section_05/pizza-menu/src/index.js
+++ Section_05/pizza-menu/src/index.js
@@ -75,27 +75,27 @@ function Menu() {
   );
 }
 
-function Pizza(props) {
-  if (props.pizzaObj.soldOut) return null;
+function Pizza({ pizzaObj }) {
+  if (pizzaObj.soldOut) return null;
 
   return (
     <li className='pizza'>
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>${props.pizzaObj.price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>${pizzaObj.price}</span>
       </div>
     </li>
   );
 }
 
-function Order(props) {
+function Order({ closeHour, openHour }) {
   return (
     <div className='order'>
       <p>
-        We're open until {props.closeHour}h! Come visit us or order online by
-        clicking on the button below.
+        We're open from {openHour}h to {closeHour}h! Come visit us or order
+        online by clicking on the button below.
       </p>
       <button className='btn'>Order</button>
     </div>
@@ -110,7 +110,7 @@ function Footer() {
   return (
     <footer className='footer'>
       {isOpen ? (
-        <Order closeHour={closeHour} />
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}h and {closeHour}h!
