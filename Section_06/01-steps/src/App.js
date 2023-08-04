941c6463b2e2f760cff1c7f7cdf10c05e3ee727b
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-08-04 12:56:18 -0300

    Final code for lesson 01

--- Section_06/01-steps/src/App.js
+++ Section_06/01-steps/src/App.js
@@ -1,3 +1,30 @@
+const messages = [
+  'Learn React ⚛️',
+  'Apply for jobs 💼',
+  'Invest your new income 🤑',
+];
+
 export default function App() {
-  return <div>Hello React</div>
+  const step = 1;
+
+  return (
+    <div className='steps'>
+      <div className='numbers'>
+        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
+        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+      </div>
+      <p className='message'>
+        Step: {step}: {messages[step - 1]}
+      </p>
+      <div className='buttons'>
+        <button style={{ backgroundColor: '#7950f2', color: '#ffffff' }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: '#7950f2', color: '#ffffff' }}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
