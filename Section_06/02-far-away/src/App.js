ea8ad9171124266b06d96d35dbf3549551f98bc1
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-08-05 05:16:54 -0300

    Final code for lesson

--- Section_06/02-far-away/src/App.js
+++ Section_06/02-far-away/src/App.js
@@ -1,7 +1,34 @@
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+
+function Form() {
+  return (
+    <div className='add-form'>
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className='list'>List</div>;
+}
+
+function Stats() {
+  return (
+    <footer className='stats'>
+      <em>You have X items on your list, and you already packed y (y%)</em>
+    </footer>
+  );
+}
+
 function App() {
   return (
     <div className='app'>
-      <h1>Hello React!</h1>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
