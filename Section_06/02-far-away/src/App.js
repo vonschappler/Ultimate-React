c8e6e471c90323c4d77cbf5742d7a576e753fe3f
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-08-05 05:36:27 -0300

    Final code for lesson

--- Section_06/02-far-away/src/App.js
+++ Section_06/02-far-away/src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: true },
+  { id: 3, description: 'Charger', quantity: 1, packed: false },
+];
+
 function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
@@ -11,7 +17,26 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className='list'>List</div>;
+  return (
+    <div className='list'>
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
