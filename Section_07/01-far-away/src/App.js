69527642d9861b07c40cbdb2c17377db66fd13d6
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-08-07 12:03:34 -0300

    Add initial files

---
+++ Section_07/01-far-away/src/App.js
@@ -0,0 +1,94 @@
+import { useState } from 'react';
+
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: true },
+  { id: 3, description: 'Charger', quantity: 1, packed: false },
+];
+
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+
+function Form() {
+  const [qty, setQty] = useState(1);
+  const [desc, setDesc] = useState('');
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    if (!desc) return;
+    const newItem = {
+      description: desc,
+      quantity: qty,
+      package: false,
+      id: Date.now(),
+    };
+    
+    console.log(newItem);
+    setDesc('');
+    setQty(1);
+  }
+  return (
+    <form className='add-form' onSubmit={handleSubmit}>
+      <h3>What do you need for your trip?</h3>
+      <select value={qty} onChange={(evt) => setQty(Number(evt.target.value))}>
+        {Array.from({ length: 20 }, (_, index) => index + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type='text'
+        placeholder='Item...'
+        value={desc}
+        onChange={(evt) => setDesc(evt.target.value)}
+      />
+      <button>Add item</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className='list'>
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <input type='checkbox' checked={item.packed} />
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className='stats'>
+      <em>You have X items on your list, and you already packed y (y%)</em>
+    </footer>
+  );
+}
+
+function App() {
+  return (
+    <div className='app'>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+export default App;
