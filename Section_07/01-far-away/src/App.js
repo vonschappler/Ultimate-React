8fa95a54667fe8aa6b244e9ee6f08c494937d8ed
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-08-07 14:37:10 -0300

    Final code for lesson 02

--- Section_07/01-far-away/src/App.js
+++ Section_07/01-far-away/src/App.js
@@ -1,11 +1,5 @@
 import { useState } from 'react';
 
-const initialItems = [
-  { id: 1, description: 'Passports', quantity: 2, packed: false },
-  { id: 2, description: 'Socks', quantity: 12, packed: true },
-  { id: 3, description: 'Charger', quantity: 1, packed: false },
-];
-
 function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
@@ -48,26 +42,26 @@ function Form({ onAddItem }) {
   );
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onDelItem }) {
   return (
     <div className='list'>
       <ul>
         {items.map((item) => (
-          <Item item={item} key={item.id} />
+          <Item item={item} key={item.id} onDelItem={onDelItem} />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDelItem }) {
   return (
     <li>
       <input type='checkbox' checked={item.packed} />
       <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDelItem(item.id)}>❌</button>
     </li>
   );
 }
@@ -85,11 +79,14 @@ function App() {
   function handleAddItem(item) {
     setItems((items) => [...items, item]);
   }
+  function handleDelItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
   return (
     <div className='app'>
       <Logo />
       <Form onAddItem={handleAddItem} />
-      <PackingList items={items} />
+      <PackingList items={items} onDelItem={handleDelItem} />
       <Stats />
     </div>
   );
