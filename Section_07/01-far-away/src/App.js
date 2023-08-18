023a790a259dbc82c2085989a0b372838800d958
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-08-18 11:02:55 -0300

    Final code for lesson 05

--- Section_07/01-far-away/src/App.js
+++ Section_07/01-far-away/src/App.js
@@ -43,10 +43,25 @@ function Form({ onAddItem }) {
 }
 
 function PackingList({ items, onDelItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState('input');
+  let sortedItems;
+  if (sortBy === 'input') {
+    sortedItems = items;
+  }
+  if (sortBy === 'description') {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+  if (sortBy === 'packed') {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
   return (
     <div className='list'>
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -55,6 +70,13 @@ function PackingList({ items, onDelItem, onToggleItem }) {
           />
         ))}
       </ul>
+      <div className='actions'>
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value='input'>Sort by input order</option>
+          <option value='description'>Sort by description</option>
+          <option value='packed'>Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
