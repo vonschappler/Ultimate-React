db48d70430827680ece198b734a3a8fd305917a3
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-08-18 14:42:46 -0300

    Final code for lesson 11

--- Section_07/04-accordion/src/App.js
+++ Section_07/04-accordion/src/App.js
@@ -25,26 +25,27 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
   return (
     <div className='accordion'>
       {data.map((el, index) => (
-        <AccordionItem title={el.title} text={el.text} number={index} key={el.title} />
+        <AccordionItem curOpen={curOpen} onOpen={setCurOpen} title={el.title} number={index} key={el.title} >{el.text}</AccordionItem>
       ))}
     </div>
   );
 }
 
-function AccordionItem({ number, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ number, title, curOpen, onOpen, children}) {
+  const isOpen = number === curOpen
   function handleToggle() {
-    setIsOpen(cur => !cur)
+    onOpen(isOpen ? null : number)
   }
   return (
     <div className={`item ${isOpen ? 'open': ''}`} onClick={handleToggle}>
       <p className='number'>{number < 9 ? `0${number + 1}` : number + 1}</p>
       <p className='title'>{title}</p>
       <p className='icon'>{isOpen ? '-' : '+' }</p>
-      {isOpen && <div className='content-box'>{text}</div>}
+      {isOpen && <div className='content-box'>{children}</div>}
     </div>
   );
 }
