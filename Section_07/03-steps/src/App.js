ea666cc352a72a6800c680c411b48b69edfbf1aa
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-08-18 13:34:03 -0300

    Final code for lesson 10

--- Section_07/03-steps/src/App.js
+++ Section_07/03-steps/src/App.js
@@ -43,9 +43,18 @@ function Steps() {
             <div className={step >= 2 ? 'active' : ''}>2</div>
             <div className={step >= 3 ? 'active' : ''}>3</div>
           </div>
-          <p className='message'>
-            Step: {step}: {messages[step - 1]}
-          </p>
+          <Content step={step}>
+            <p>{messages[step - 1]}</p>
+            <div className='buttons'>
+              <Button
+                bgColor='#cccccc'
+                textColor='#7950f2'
+                onClick={() => alert(messages[step - 1])}
+              >
+                Learn how
+              </Button>
+            </div>
+          </Content>
           <div className='buttons'>
             <Button
               bgColor='#7950f2'
@@ -74,5 +83,14 @@ function Button({ textColor, bgColor, onClick, children }) {
     >
       {children}
     </button>
+  );
+}
+
+function Content({ step, children }) {
+  return (
+    <div className='message'>
+      <h3>Step {step}</h3>
+      {children}
+    </div>
   );
 }
