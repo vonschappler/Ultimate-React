cbab5a6d5d196ee59f9b3ebdad3f50860ee21252
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-08-18 18:02:26 -0300

    Final code for lesson 01

--- Section_08/01-eat-n-split/src/App.js
+++ Section_08/01-eat-n-split/src/App.js
@@ -1,20 +1,68 @@
 const initialFriends = [
   {
     id: 118836,
-    name: "Clark",
-    image: "https://i.pravatar.cc/48?u=118836",
+    name: 'Clark',
+    image: 'https://i.pravatar.cc/48?u=118836',
     balance: -7,
   },
   {
     id: 933372,
-    name: "Sarah",
-    image: "https://i.pravatar.cc/48?u=933372",
+    name: 'Sarah',
+    image: 'https://i.pravatar.cc/48?u=933372',
     balance: 20,
   },
   {
     id: 499476,
-    name: "Anthony",
-    image: "https://i.pravatar.cc/48?u=499476",
+    name: 'Anthony',
+    image: 'https://i.pravatar.cc/48?u=499476',
     balance: 0,
   },
 ];
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className='red'>
+          You owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && (
+        <p>
+          You and {friend.name} are even
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className='green'>
+          {friend.name} oew you ${Math.abs(friend.balance)}
+        </p>
+      )}
+      <button className="button">Select</button>
+    </li>
+  );
+}
+
+function FriendsList() {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function App() {
+  return (
+    <div className='app'>
+      <div className='sidebar'>
+        <FriendsList />
+      </div>
+    </div>
+  );
+}
+
+export default App;
