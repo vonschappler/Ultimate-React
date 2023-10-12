11aedbd3524c48eafd64e5d1ce89f840371eddbc
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-10-12 10:44:11 -0300

    Initial commit

---
+++ Section_12/01-use-popcorn/src/index.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import StarRating from './StarRating';
+// import './index.css';
+// import App from './App';
+
+function TestRating() {
+  const [movieRating, setMovieRating] = useState(0)
+  return <div>
+    <StarRating color='blue' maxRating={10} onSetRating={setMovieRating} />
+    <p>This movie was rated {movieRating} stars</p>
+  </div>
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+    />
+    <StarRating size={24} color='red' defaultRating={3} />
+    <TestRating />
+  </React.StrictMode>
+);
