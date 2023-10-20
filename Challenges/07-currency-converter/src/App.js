b89c95fff1ac3f469d65e10a39a041d83e899822
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-10-20 11:38:20 -0300

    My solution

--- Challenges/07-currency-converter/src/App.js
+++ Challenges/07-currency-converter/src/App.js
@@ -1,22 +1,50 @@
-// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [value, setValue] = useState('0');
+  const [input, setInput] = useState('USD');
+  const [output, setOutput] = useState('USD');
+  const [converted, setConverted] = useState('0');
+
+  useEffect(
+    function () {
+      const fetchConversion = async () => {
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${value}&from=${input}&to=${output}`
+        );
+        console.log(res);
+        const data = await res.json();
+        setConverted(data.rates[output]);
+      };
+      if (!value || value === '0') return;
+      if (input === output) return;
+      fetchConversion();
+    },
+    [value, input, output]
+  );
+
   return (
     <div>
-      <input type='text' />
-      <select>
+      <input
+        type='text'
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <select value={input} onChange={(e) => setInput(e.target.value)}>
         <option value='USD'>USD</option>
         <option value='EUR'>EUR</option>
         <option value='CAD'>CAD</option>
         <option value='INR'>INR</option>
       </select>
-      <select>
+      <select value={output} onChange={(e) => setOutput(e.target.value)}>
         <option value='USD'>USD</option>
         <option value='EUR'>EUR</option>
         <option value='CAD'>CAD</option>
         <option value='INR'>INR</option>
       </select>
-      <p>OUTPUT</p>
+      <p>
+        ${value} {input} converted to {output} is equal ${converted}
+      </p>
     </div>
   );
 }
