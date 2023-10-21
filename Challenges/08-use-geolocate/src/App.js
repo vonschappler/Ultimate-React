cf233082ca2d40aa4fd821a7c907d84028195bec
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-10-21 14:54:48 -0300

    Setup project

--- Challenges/08-use-geolocate/src/App.js
+++ Challenges/08-use-geolocate/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+function useGeolocation() {}
+
+export default function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [countClicks, setCountClicks] = useState(0);
+  const [position, setPosition] = useState({});
+  const [error, setError] = useState(null);
+
+  const { lat, lng } = position;
+
+  function getPosition() {
+    setCountClicks((count) => count + 1);
+
+    if (!navigator.geolocation)
+      return setError('Your browser does not support geolocation');
+
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <button onClick={getPosition} disabled={isLoading}>
+        Get my position
+      </button>
+
+      {isLoading && <p>Loading position...</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && !error && lat && lng && (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Your GPS position:{' '}
+          <a
+            target='_blank'
+            rel='noreferrer'
+            href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
+          >
+            {lat}, {lng}
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      )}
+
+      <p>You requested position {countClicks} times</p>
     </div>
   );
 }
-
-export default App;
