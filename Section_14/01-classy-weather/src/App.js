35d9f9cbf131082676e5de025cc86d63b873df22
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-10-22 09:43:42 -0300

    Final code for lesson 01

--- Section_14/01-classy-weather/src/App.js
+++ Section_14/01-classy-weather/src/App.js
@@ -1,10 +1,23 @@
-import './index.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class Counter extends React.Component {
+  // the consstructor method of a class component
+  constructor(props) {
+    super(props)
+
+    // initializes states, which needs to be an object
+    this.state = {count: 5}
+  }
+  // this is equivalent of the function body
+  render() {
+    return (
+      <div>
+        <button>-</button>
+        <span>{this.state.count}</span>
+        <button>+</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Counter;
