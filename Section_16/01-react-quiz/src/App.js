95095a7eb9fe22f07917ba51ac1951c48492aec2
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-10-29 11:59:53 -0300

    Final code for lesson 04

--- Section_16/01-react-quiz/src/App.js
+++ Section_16/01-react-quiz/src/App.js
@@ -1,7 +1,34 @@
+import { useEffect, useReducer } from 'react';
 import Header from './Header';
 import Main from './Main';
 
+const initialState = {
+  questions: [],
+
+  // loading, error, ready, active or finished
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...state, question: action.payload, status: 'ready' };
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    default:
+      throw new Error('Action unknow');
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch('http://localhost:8000/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch((err) => dispatch({type: 'dataFailed'}));
+  }, []);
   return (
     <div className='app'>
       <Header />
