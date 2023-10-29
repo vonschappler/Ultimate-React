e2099f2805b0548e743e6b58902f0444fb9d8c6a
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-10-29 11:38:48 -0300

    Final code for lesson 03

--- Section_16/01-react-quiz/src/App.js
+++ Section_16/01-react-quiz/src/App.js
@@ -1,7 +1,14 @@
-import DateCounter from "./DateCounter";
+import Header from './Header';
+import Main from './Main';
 
 export default function App() {
-  return <div>
-    <DateCounter />
-  </div>
+  return (
+    <div className='app'>
+      <Header />
+      <Main className='main'>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  );
 }
---
+++ Section_16/01-react-quiz/src/Main.js
@@ -0,0 +1,9 @@
+function Main({children}) {
+  return (
+    <main className="main">
+      {children}
+    </main>
+  )
+}
+
+export default Main
