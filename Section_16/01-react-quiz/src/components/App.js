caaf25214ebc9ff21150167b84d4bdb083164b5d
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-10-29 13:31:03 -0300

    Final code for lesson 08

--- Section_16/01-react-quiz/src/components/App.js
+++ Section_16/01-react-quiz/src/components/App.js
@@ -12,6 +12,8 @@ const initialState = {
   // loading, error, ready, active or finished
   status: 'loading',
   index: 0,
+  answer: null,
+  points: 0
 };
 
 function reducer(state, action) {
@@ -22,13 +24,17 @@ function reducer(state, action) {
       return { ...state, status: 'error' };
     case 'start':
       return { ...state, status: 'active' };
+    case 'newAnswer':
+      const question = state.questions.at(state.index)
+
+      return {...state, answer: action.payload, points: action.payload === question.correctOption ? state.points + question.points : state.points}
     default:
       throw new Error('Action unknow');
   }
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -51,7 +57,7 @@ export default function App() {
         {status === 'ready' && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === 'active' && <Question question={questions.at(index)} />}
+        {status === 'active' && <Question question={questions.at(index)} dispatch={dispatch} answer={answer} points={points} />}
       </Main>
     </div>
   );
--- Section_16/01-react-quiz/src/components/Options.js
+++ Section_16/01-react-quiz/src/components/Options.js
@@ -1,8 +1,17 @@
-function Options({question}) {
+function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null
   return (
     <div className='options'>
-      {question.options.map((opt) => (
-        <button className='btn btn-option' key={opt}>
+      {question.options.map((opt, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? 'answer' : ''} ${hasAnswered
+            ? index === question.correctOption
+              ? 'correct'
+              : 'wrong' : ""}`}
+          key={opt}
+          onClick={() => dispatch({ type: 'newAnswer', payload: index })}
+          disabled={hasAnswered}
+        >
           {opt}
         </button>
       ))}
@@ -10,4 +19,4 @@ function Options({question}) {
   );
 }
 
-export default Options
+export default Options;
