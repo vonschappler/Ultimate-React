cfa658147ef6f33ad9d0d98e749073bb315d46fe
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-11-02 11:33:01 -0300

    Final code for lesson 04

--- Section_18/02-world-wise/src/App.jsx
+++ Section_18/02-world-wise/src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import AppLayout from './pages/AppLayout';
 import City from './components/City';
@@ -10,51 +9,31 @@ import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
 import Pricing from './pages/Pricing';
 import Product from './pages/Product';
+import { CitiesProvider } from './contexts/CitiesContext';
 
-const BASE_URL = `http://localhost:8000`;
+
 
 function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsloading] = useState(false);
 
-  useEffect(function () {
-    async function fetchCities() {
-      try {
-        setIsloading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        setCities(data);
-      } catch (err) {
-        alert('There was an error loading data...');
-      } finally {
-        setIsloading(false);
-      }
-    }
-    fetchCities();
-  }, []);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Homepage />} />
-        <Route path='product' element={<Product />} />
-        <Route path='pricing' element={<Pricing />} />
-        <Route path='login' element={<Login />} />
-        <Route path='app' element={<AppLayout />}>
-          <Route index element={<Navigate replace to='cities' />} />
-          <Route
-            path='cities'
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
-          <Route path='cities/:id' element={<City />} />
-          <Route
-            path='countries'
-            element={<CountryList cities={cities} isLoading={isLoading} />}
-          />
-          <Route path='form' element={<Form />} />
-        </Route>
-        <Route path='*' element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <CitiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path='product' element={<Product />} />
+          <Route path='pricing' element={<Pricing />} />
+          <Route path='login' element={<Login />} />
+          <Route path='app' element={<AppLayout />}>
+            <Route index element={<Navigate replace to='cities' />} />
+            <Route path='cities' element={<CityList />} />
+            <Route path='cities/:id' element={<City />} />
+            <Route path='countries' element={<CountryList />} />
+            <Route path='form' element={<Form />} />
+          </Route>
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </CitiesProvider>
   );
 }
 
---
+++ Section_18/02-world-wise/src/contexts/CitiesContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useState, useEffect } from 'react';
+
+const BASE_URL = `http://localhost:8000`;
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsloading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsloading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (err) {
+        alert('There was an error loading data...');
+      } finally {
+        setIsloading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+export { CitiesProvider};
