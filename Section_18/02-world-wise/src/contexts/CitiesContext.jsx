494efc5c68b9374c566423b5c93d8a8b62d3a2b6
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-11-02 11:39:42 -0300

    Final code for lesson 05

--- Section_18/02-world-wise/src/contexts/CitiesContext.jsx
+++ Section_18/02-world-wise/src/contexts/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect, useContext } from 'react';
 
 const BASE_URL = `http://localhost:8000`;
 
@@ -35,4 +35,10 @@ function CitiesProvider({ children }) {
   );
 }
 
-export { CitiesProvider};
+function useCities() {
+  const context = useContext(CitiesContext)
+  if (context === undefined) throw new Error ('CitiesContext Context used out of scope')
+  return context
+}
+
+export { CitiesProvider, useCities};
