30300e5ed3683f2b10bfd7b87b0f936b6e08de8d
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-12-13 13:08:52 -0300

    Final code for challenge part 2

--- Challenges/11-workout-timer/src/Calculator.js
+++ Challenges/11-workout-timer/src/Calculator.js
@@ -1,4 +1,4 @@
-import { useState, memo} from 'react';
+import { useState, memo, useEffect} from 'react';
 import clickSound from './ClickSound.m4a';
 
 function Calculator({ workouts, allowSound }) {
@@ -6,8 +6,13 @@ function Calculator({ workouts, allowSound }) {
   const [sets, setSets] = useState(3);
   const [speed, setSpeed] = useState(90);
   const [durationBreak, setDurationBreak] = useState(5);
+  const [duration, setDuration] = useState(0)
 
-  const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+  useEffect(() => {
+    setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
+  }, [number, sets, speed, durationBreak])
+
+  // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
 
@@ -16,6 +21,14 @@ function Calculator({ workouts, allowSound }) {
     const sound = new Audio(clickSound);
     sound.play();
   };
+
+  function handleDec() {
+    setDuration(duration => duration > 1 ? Math.ceil(duration) - 1 : 0)
+  }
+
+  function handleInc() {
+    setDuration(duration => Math.floor(duration) + 1)
+  }
 
   return (
     <>
@@ -66,13 +79,13 @@ function Calculator({ workouts, allowSound }) {
         </div>
       </form>
       <section>
-        <button onClick={() => {}}>–</button>
+        <button onClick={handleDec}>–</button>
         <p>
           {mins < 10 && '0'}
           {mins}:{seconds < 10 && '0'}
           {seconds}
         </p>
-        <button onClick={() => {}}>+</button>
+        <button onClick={handleInc}>+</button>
       </section>
     </>
   );
