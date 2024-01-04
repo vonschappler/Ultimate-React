2b8ff7f5ecf3a42c7840a88fab6f4ce2a3da271c
von Schappler <37213588+vonschappler@users.noreply.github.com>
2024-01-04 16:06:30 -0300

    Final code for lesson 03

--- Section_20/01-redux-intro/src/store.js
+++ Section_20/01-redux-intro/src/store.js
@@ -35,19 +35,36 @@ function reducer(state = initialState, action) {
 
 const store = createStore(reducer);
 
-store.dispatch({ type: 'account/deposit', payload: 500 });
+function deposit(amount) {
+  return { type: 'account/deposit', payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: 'account/withdraw', payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return {
+    type: 'account/requestLoan',
+    payload: {
+      amount,
+      purpose,
+    },
+  };
+}
+
+function payLoan() {
+  return { type: 'account/payLoan' };
+}
+
+store.dispatch(deposit(500));
 console.log(store.getState());
 
-store.dispatch({ type: 'account/withdraw', payload: 200 });
+store.dispatch(withdraw(200));
 console.log(store.getState());
 
-store.dispatch({
-  type: 'account/requestLoan',
-  payload: { amount: 1000, purpose: 'Buy a car' },
-});
+store.dispatch(requestLoan(1000, 'Buy a extremely cheap car'));
 console.log(store.getState());
 
-store.dispatch({
-  type: 'account/payLoan',
-});
+store.dispatch(payLoan());
 console.log(store.getState());
