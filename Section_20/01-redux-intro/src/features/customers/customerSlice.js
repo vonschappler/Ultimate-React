093a2272e5b33b55228d35e8593d757830028553
von Schappler <37213588+vonschappler@users.noreply.github.com>
2024-01-04 17:29:35 -0300

    Final code for lesson 05

---
+++ Section_20/01-redux-intro/src/features/customers/customerSlice.js
@@ -0,0 +1,36 @@
+const inititalStateCustomer = {
+  fullName: '',
+  nationalId: '',
+  createdAt: '',
+};
+
+export default function customerReducer(state = inititalStateCustomer, action) {
+  switch (action.type) {
+    case 'customer/createCustomer':
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalId: action.payload.nationalId,
+        createdAt: action.payload.createdAt,
+      };
+    case 'customer/updateName':
+      return { ...state, fullName: action.payload };
+    default:
+      return state;
+  }
+}
+
+export function createCustomer(fullName, nationalId) {
+  return {
+    type: 'customer/createCustomer',
+    payload: {
+      fullName,
+      nationalId,
+      createdAt: new Date().toISOString(),
+    },
+  };
+}
+
+export function updateName(fullName) {
+  return { type: 'customer/updateName', payload: fullName };
+}
