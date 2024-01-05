54038309d1f4d58e746bfdecb4217e104d78ba05
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-01-05 12:18:25 -0300

    Final code for lesson 08

--- Section_20/01-redux-intro/src/features/accounts/BalanceDisplay.js
+++ Section_20/01-redux-intro/src/features/accounts/BalanceDisplay.js
@@ -1,3 +1,7 @@
+// This file is made using the "old way" before hooks were implemented in React
+
+import { connect } from 'react-redux';
+
 function formatCurrency(value) {
   return new Intl.NumberFormat('en', {
     style: 'currency',
@@ -5,8 +9,16 @@ function formatCurrency(value) {
   }).format(value);
 }
 
-function BalanceDisplay() {
-  return <div className='balance'>{formatCurrency(123456)}</div>;
+function BalanceDisplay({ balance }) {
+  return <div className='balance'>{formatCurrency(balance)}</div>;
 }
 
-export default BalanceDisplay;
+// This function returns an object with the "props" which we are creating for a component
+function mapStateToProps(state) {
+  return {
+    balance: state.account.balance,
+  };
+}
+
+// This function creates a new component, which will receive the props mapped by mapStateToProps and apply them to the component passed as the argument of connect()()
+export default connect(mapStateToProps)(BalanceDisplay);
