13ec957a1c274ea438259ef0c0d1daab85e48b23
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-01-06 15:30:10 -0300

    Final code for lesson 01

--- Section_22/01-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ Section_22/01-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -9,21 +9,21 @@ const isValidPhone = (str) =>
 const fakeCart = [
   {
     pizzaId: 12,
-    name: "Mediterranean",
+    name: 'Mediterranean',
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: "Vegetale",
+    name: 'Vegetale',
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: "Spinach and Mushroom",
+    name: 'Spinach and Mushroom',
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -36,37 +36,37 @@ function CreateOrder() {
 
   return (
     <div>
-      <h2>Ready to order? Let's go!</h2>
+      <h2>Ready to order? Let&apos;s go!</h2>
 
       <form>
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input type='text' name='customer' required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input type='tel' name='phone' required />
           </div>
         </div>
 
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input type='text' name='address' required />
           </div>
         </div>
 
         <div>
           <input
-            type="checkbox"
-            name="priority"
-            id="priority"
+            type='checkbox'
+            name='priority'
+            id='priority'
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label htmlFor='priority'>Want to yo give your order priority?</label>
         </div>
 
         <div>
