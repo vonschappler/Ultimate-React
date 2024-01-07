51d3f65dc85d3fd05013729c7602b6292598e991
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-01-07 13:04:44 -0300

    Final code for lesson 06

--- Section_22/01-fast-react-pizza/src/App.jsx
+++ Section_22/01-fast-react-pizza/src/App.jsx
@@ -4,7 +4,7 @@ import Error from './ui/Error';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
-import Order from './features/order/Order';
+import Order, { loader as orderLoader } from './features/order/Order';
 import AppLayout from './ui/AppLayout';
 
 const router = createBrowserRouter([
@@ -30,7 +30,12 @@ const router = createBrowserRouter([
         path: '/order/new',
         element: <CreateOrder />,
       },
-      { path: '/order/:orderId', element: <Order /> },
+      {
+        path: '/order/:orderId',
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
     ],
   },
 ]);
--- Section_22/01-fast-react-pizza/src/ui/Header.jsx
+++ Section_22/01-fast-react-pizza/src/ui/Header.jsx
@@ -1,12 +1,14 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
+import SeachOrder from '../features/order/SeachOrder';
 
 function Header() {
   return (
     <header>
       <Link to='/'>Fast React Pizza Co.</Link>
+      <SeachOrder />
       <p>Username</p>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
