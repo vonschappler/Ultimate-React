ee6e4b31a258f5c5f1d76b0fe271e0b4e25e8f67
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-01-07 11:23:27 -0300

    Final code for lesson 03

--- Section_22/01-fast-react-pizza/src/features/menu/Menu.jsx
+++ Section_22/01-fast-react-pizza/src/features/menu/Menu.jsx
@@ -1,5 +1,21 @@
+import { useLoaderData } from 'react-router-dom';
+import MenuItem from './MenuItem';
+import { getMenu } from '../../services/apiRestaurant';
+
 function Menu() {
-  return <h1>Menu</h1>;
+  const pizzas = useLoaderData();
+  return (
+    <ul>
+      {pizzas.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
+}
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
