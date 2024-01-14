0426bcf9f1b1eed10ba8568222ce9768a260d355
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-01-14 14:56:39 -0300

    Final code for lesson 06

--- Section_23/01-fast-react-pizza/src/ui/AppLayout.jsx
+++ Section_23/01-fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,18 +1,20 @@
-import { Outlet, useNavigation } from 'react-router-dom';
-import CartOverview from '../features/cart/CartOverview';
-import Header from './Header';
-import Loader from './Loader';
+import { Outlet, useNavigation } from "react-router-dom";
+import CartOverview from "../features/cart/CartOverview";
+import Header from "./Header";
+import Loader from "./Loader";
 
 function AppLayout() {
   const navigation = useNavigation();
-  const isLoading = navigation.state === 'loading';
+  const isLoading = navigation.state === "loading";
   return (
-    <div className='layout'>
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
       <Header />
-      <main>
-        <Outlet />
-      </main>
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
       <CartOverview />
     </div>
   );
