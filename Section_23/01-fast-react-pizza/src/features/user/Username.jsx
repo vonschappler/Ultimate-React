d38c8117600fbfd1f42dc78cb85686469b6dfb2c
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-01-14 12:50:59 -0300

    Final code for lesson 03

--- Section_23/01-fast-react-pizza/src/features/user/Username.jsx
+++ Section_23/01-fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Username</div>;
+  return <div className="hidden text-sm font-semibold">Username</div>;
 }
 
 export default Username;
