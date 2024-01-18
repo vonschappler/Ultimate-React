f5af00aeb1f1ae4341f51b4edffee56a5fb6ccfd
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-01-18 14:05:58 -0300

    Final code for lesson 11

--- Section_23/01-fast-react-pizza/src/ui/Loader.jsx
+++ Section_23/01-fast-react-pizza/src/ui/Loader.jsx
@@ -1,9 +1,9 @@
 function Loader() {
   return (
-    <div className="loader">
-      
+    <div className="absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm">
+      <div className="loader"></div>
     </div>
-  )
+  );
 }
 
-export default Loader
+export default Loader;
