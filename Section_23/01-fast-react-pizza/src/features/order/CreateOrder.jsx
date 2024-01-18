830f6d24ba5f6b05ae353b3e398edb53d3552afc
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-01-18 13:27:01 -0300

    Final code for lesson 10

--- Section_23/01-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ Section_23/01-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Form, redirect, useNavigation, useActionData } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
+import Button from "../../ui/Button";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -67,7 +68,7 @@ function CreateOrder() {
 
         <div>
           <input
-            className="focus:ring-yellow h-6 w-6 accent-yellow-400 outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
+            className="input"
             type="checkbox"
             name="priority"
             id="priority"
@@ -79,12 +80,9 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button
-            disabled={isSubmitting}
-            className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 outline-none transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
-          >
+          <Button disabled={isSubmitting}>
             {isSubmitting ? `Processing your order` : `Order now`}
-          </button>
+          </Button>
         </div>
       </Form>
     </div>
--- Section_23/01-fast-react-pizza/src/ui/AppLayout.jsx
+++ Section_23/01-fast-react-pizza/src/ui/AppLayout.jsx
@@ -10,7 +10,7 @@ function AppLayout() {
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
       <Header />
-      <div className="overflow-scroll">
+      <div className="overflow-y-auto overflow-x-hidden">
         <main className="mx-auto max-w-3xl">
           <Outlet />
         </main>
