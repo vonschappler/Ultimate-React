9fdfd932f55d36e14713caf06e96db3dabb18556
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-01-19 08:58:41 -0300

    Final code for lesson 02

--- Section_24/01-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ Section_24/01-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Form, redirect, useNavigation, useActionData } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -38,6 +39,9 @@ function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
   const formErrors = useActionData();
+
+  const username = useSelector((state) => state.user.username);
+
   const cart = fakeCart;
 
   return (
@@ -49,7 +53,13 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name:</label>
-          <input type="text" name="customer" required className="input grow" />
+          <input
+            type="text"
+            name="customer"
+            required
+            className="input grow"
+            defaultValue={username}
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
