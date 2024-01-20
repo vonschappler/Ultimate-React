0be9cd61673197e3c7ae1fee13dfb2a78b00b489
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-01-20 14:18:49 -0300

    Final code for lesson 06

--- Section_25/01-wild-oasis/src/App.jsx
+++ Section_25/01-wild-oasis/src/App.jsx
@@ -9,6 +9,7 @@ import PageNotFound from './pages/PageNotFound';
 import Settings from './pages/Settings';
 import Users from './pages/Users';
 import GlobalStyles from './styles/GlobalStyles';
+import AppLayout from './ui/AppLayout';
 
 function App() {
   return (
@@ -16,13 +17,15 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to='dashboard' />} />
-          <Route path='dashboard' element={<Dashboard />} />
-          <Route path='account' element={<Account />} />
-          <Route path='bookings' element={<Bookings />} />
-          <Route path='cabins' element={<Cabins />} />
-          <Route path='login' element={<Login />} />
-          <Route path='settings' element={<Settings />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to='dashboard' />} />
+            <Route path='dashboard' element={<Dashboard />} />
+            <Route path='account' element={<Account />} />
+            <Route path='bookings' element={<Bookings />} />
+            <Route path='cabins' element={<Cabins />} />
+            <Route path='login' element={<Login />} />
+            <Route path='settings' element={<Settings />} />
+          </Route>
           <Route path='users' element={<Users />} />
           <Route path='*' element={<PageNotFound />} />
         </Routes>
