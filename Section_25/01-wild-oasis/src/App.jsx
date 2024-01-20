5e99f84ec69e30b2937258346b03fc25757ffa61
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-01-20 12:50:04 -0300

    Final code for lesson 02

--- Section_25/01-wild-oasis/src/App.jsx
+++ Section_25/01-wild-oasis/src/App.jsx
@@ -1,28 +1,7 @@
 import styled from 'styled-components';
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  cursor: pointer;
-  margin: 0px 20px;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-`;
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
 
 const StyledApp = styled.div`
   background-color: orangered;
@@ -31,12 +10,14 @@ const StyledApp = styled.div`
 
 function App() {
   return (
-    <StyledApp>
-      <H1>The Wild Oasis</H1>
-      <Button onClick={() => alert('Check in')}>Check in</Button>
-      <Button onClick={() => alert('Check out')}>Check out</Button>
-      <Input type='number' placeholder='Number of guests' />
-    </StyledApp>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Button onClick={() => alert('Check in')}>Check in</Button>
+        <Button onClick={() => alert('Check out')}>Check out</Button>
+        <Input type='number' placeholder='Number of guests' />
+      </StyledApp>
+    </>
   );
 }
 
--- Section_25/01-wild-oasis/src/ui/Button.jsx
+++ Section_25/01-wild-oasis/src/ui/Button.jsx
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled, { css } from 'styled-components';
 
 const sizes = {
   small: css`
@@ -47,3 +47,20 @@ const variations = {
     }
   `,
 };
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: var(--border-radius-sm);
+  background-color: var(--color-brand-600);
+  color: var(--color-brand-50);
+  box-shadow: var(--shadow-sm);
+  cursor: pointer;
+  &:hover {
+    background-color: var(--color-brand-700);
+  }
+`;
+
+export default Button;
