26cd25ff87f5910c32300eed7a3f4f4a33f3af98
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-01-20 13:59:17 -0300

    Final code for lesson 05

--- Section_25/01-wild-oasis/src/App.jsx
+++ Section_25/01-wild-oasis/src/App.jsx
@@ -1,43 +1,32 @@
-import styled from 'styled-components';
-import GlobalStyles from './styles/GlobalStyles';
-import Button from './ui/Button';
-import Input from './ui/Input';
-import Heading from './ui/Heading';
-import Row from './ui/Row';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
-const StyledApp = styled.div`
-  padding: 20px;
-`;
+import Account from './pages/Account';
+import Bookings from './pages/Bookings';
+import Cabins from './pages/Cabins';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
+import Settings from './pages/Settings';
+import Users from './pages/Users';
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <StyledApp>
-        <Row>
-          <Row type='horizontal'>
-            <Heading as='h1'>The Wild Oasis</Heading>
-            <div>
-              <Heading as='h2'>Check in and out</Heading>
-              <Button onClick={() => alert('Check in')}>Check in</Button>
-              <Button
-                variation='secondary'
-                size='small'
-                onClick={() => alert('Check out')}
-              >
-                Check out
-              </Button>
-            </div>
-          </Row>
-          <Row>
-            <Heading as='h3'>Form</Heading>
-            <form>
-              <Input type='number' placeholder='Number of guests' />
-              <Input type='text' placeholder='Number of guests' />
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to='dashboard' />} />
+          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='account' element={<Account />} />
+          <Route path='bookings' element={<Bookings />} />
+          <Route path='cabins' element={<Cabins />} />
+          <Route path='login' element={<Login />} />
+          <Route path='settings' element={<Settings />} />
+          <Route path='users' element={<Users />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
