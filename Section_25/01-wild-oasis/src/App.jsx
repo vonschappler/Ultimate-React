0747196ce18b5fe90f428b6fe44d7931e19a79b6
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-01-20 13:19:41 -0300

    Final code for lesson 03

--- Section_25/01-wild-oasis/src/App.jsx
+++ Section_25/01-wild-oasis/src/App.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
 import Button from './ui/Button';
 import Input from './ui/Input';
+import Heading from './ui/Heading';
 
 const StyledApp = styled.div`
   background-color: orangered;
@@ -13,9 +14,13 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
+        <Heading as='h1'>The Wild Oasis</Heading>
+        <Heading as='h2'>Buttons</Heading>
         <Button onClick={() => alert('Check in')}>Check in</Button>
         <Button onClick={() => alert('Check out')}>Check out</Button>
+        <Heading as='h3'>Inputs</Heading>
         <Input type='number' placeholder='Number of guests' />
+        <Input type='text' placeholder='Number of guests' />
       </StyledApp>
     </>
   );
