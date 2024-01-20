84f3bdff40e07b9c0e89c1a5b531bd937066cbd4
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-01-20 13:39:25 -0300

    Final code for lesson 04

--- Section_25/01-wild-oasis/src/App.jsx
+++ Section_25/01-wild-oasis/src/App.jsx
@@ -3,9 +3,9 @@ import GlobalStyles from './styles/GlobalStyles';
 import Button from './ui/Button';
 import Input from './ui/Input';
 import Heading from './ui/Heading';
+import Row from './ui/Row';
 
 const StyledApp = styled.div`
-  background-color: orangered;
   padding: 20px;
 `;
 
@@ -14,13 +14,29 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as='h1'>The Wild Oasis</Heading>
-        <Heading as='h2'>Buttons</Heading>
-        <Button onClick={() => alert('Check in')}>Check in</Button>
-        <Button onClick={() => alert('Check out')}>Check out</Button>
-        <Heading as='h3'>Inputs</Heading>
-        <Input type='number' placeholder='Number of guests' />
-        <Input type='text' placeholder='Number of guests' />
+        <Row>
+          <Row type='horizontal'>
+            <Heading as='h1'>The Wild Oasis</Heading>
+            <div>
+              <Heading as='h2'>Check in and out</Heading>
+              <Button onClick={() => alert('Check in')}>Check in</Button>
+              <Button
+                variation='secondary'
+                size='small'
+                onClick={() => alert('Check out')}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as='h3'>Form</Heading>
+            <form>
+              <Input type='number' placeholder='Number of guests' />
+              <Input type='text' placeholder='Number of guests' />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
