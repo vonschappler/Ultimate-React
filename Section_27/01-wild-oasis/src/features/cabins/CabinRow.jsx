84c1c11f21820d25c0aa91e579585f508a030a15
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-01-22 13:33:31 -0300

    Final code for lesson 04

--- Section_27/01-wild-oasis/src/features/cabins/CabinRow.jsx
+++ Section_27/01-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { formatCurrency } from '../../utils/helpers';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { deleteCabin } from '../../services/apiCabins';
+import toast from 'react-hot-toast';
 
 const TableRow = styled.div`
   display: grid;
@@ -58,12 +59,12 @@ function CabinRow({ cabin }) {
   const { isLoading: isDeleting, mutate } = useMutation({
     mutationFn: deleteCabin,
     onSuccess: () => {
-      alert('Cabin successfully deleted!');
+      toast.success('Cabin successfully deleted!');
       queryClient.invalidateQueries({
         queryKey: ['cabins'],
       });
     },
-    onError: (err) => alert(err.message),
+    onError: (err) => toast.error(err.message),
   });
 
   return (
