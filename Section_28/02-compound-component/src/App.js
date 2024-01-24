5021f7a794e7b0070c8c82ee1e2a36b50f78df10
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-01-24 13:15:42 -0300

    Final code for lesson 03

--- Section_28/02-compound-component/src/App.js
+++ Section_28/02-compound-component/src/App.js
@@ -5,14 +5,23 @@ export default function App() {
   return (
     <div>
       <h1>Compound Component Pattern</h1>
-      <Counter
-        iconIncrease='+'
-        iconDecrease='-'
-        label='My NOT so flexible counter'
-        hideLabel={false}
-        hideIncrease={false}
-        hideDecrease={false}
-      />
+
+      <div>
+        <Counter>
+          <Counter.Decrease icon='-' />
+          <Counter.Count />
+          <Counter.Increase icon='+' />
+          <Counter.Label>Compound Counter</Counter.Label>
+        </Counter>
+      </div>
+
+      <div>
+        <Counter>
+          <Counter.Decrease icon='⬅️' />
+          <Counter.Count />
+          <Counter.Increase icon='➡️' />
+        </Counter>
+      </div>
     </div>
   );
 }
--- Section_28/02-compound-component/src/Counter.js
+++ Section_28/02-compound-component/src/Counter.js
@@ -1,4 +1,39 @@
-function Counter() {
-  return <span>Counter</span>;
+import { createContext, useContext, useState } from 'react';
+
+const ConterContext = createContext();
+
+function Counter({ children }) {
+  const [count, setCount] = useState(0);
+
+  const increase = () => setCount((c) => c + 1);
+  const decrease = () => setCount((c) => c - 1);
+
+  return (
+    <ConterContext.Provider value={{ count, increase, decrease }}>
+      <span>{children}</span>
+    </ConterContext.Provider>
+  );
 }
 export default Counter;
+
+function Count() {
+  const { count } = useContext(ConterContext);
+  return <span>{count}</span>;
+}
+function Label({ children }) {
+  return <span>{children}</span>;
+}
+
+function Increase({ icon }) {
+  const { increase } = useContext(ConterContext);
+  return <button onClick={increase}>{icon}</button>;
+}
+function Decrease({ icon }) {
+  const { decrease } = useContext(ConterContext);
+  return <button onClick={decrease}>{icon}</button>;
+}
+
+Counter.Count = Count;
+Counter.Label = Label;
+Counter.Increase = Increase;
+Counter.Decrease = Decrease;
