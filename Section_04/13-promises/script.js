535be14dcecce45f657fef0fadcc94b1dfd4cd1f
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-01-25 14:18:56 -0300

    Fix errors to console.error

--- Section_04/13-promises/script.js
+++ Section_04/13-promises/script.js
@@ -6,7 +6,7 @@ const API = 'https://jsonplaceholder.typicode.com/todos';
 fetch(API)
   .then((res) => res.json())
   .then((data) => console.log(data))
-  .catch(console.log('Some error handler message or function...'));
+  .catch(console.error('Some error handler message or function...'));
 
 console.log(
   'This will happen fisrt because the promise is still running on background and being processed.\n\nPromisses are asyncronous'
