87eb3bd13846917c181cfec1d116dff4ada2cc83
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-01-25 14:26:55 -0300

    Removal of unwanted console.log and fix errors to console.error

--- Section_26/01-wild-oasis/src/services/apiCabins.js
+++ Section_26/01-wild-oasis/src/services/apiCabins.js
@@ -3,7 +3,7 @@ import supabase from './supabase';
 export async function getCabins() {
   const { data, error } = await supabase.from('cabins').select('*');
   if (error) {
-    console.log(error);
+    console.error(error);
     throw new Error('Cabins could not be loaded');
   }
   return data;
