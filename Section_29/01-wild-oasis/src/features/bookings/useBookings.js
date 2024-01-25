c0974867981c55ebb9c9f2d23af7804da86ffa4e
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-01-25 15:04:14 -0300

    Final code for lesson 05

--- Section_29/01-wild-oasis/src/features/bookings/useBookings.js
+++ Section_29/01-wild-oasis/src/features/bookings/useBookings.js
@@ -1,14 +1,22 @@
 import { useQuery } from '@tanstack/react-query';
 import { getBookings as getBookingsApi } from '../../services/apiBookings';
+import { useSearchParams } from 'react-router-dom';
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+  const filterValue = searchParams.get('status');
+  const filter =
+    !filterValue || filterValue === 'all'
+      ? null
+      : { field: 'status', value: filterValue };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ['bookings'],
-    queryFn: getBookingsApi,
+    queryKey: ['bookings', filter],
+    queryFn: () => getBookingsApi({ filter }),
   });
 
   return { isLoading, bookings, error };
