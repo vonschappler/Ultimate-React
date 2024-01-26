0da144e6127d276e3418d5cfe4216158db851387
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-01-26 18:24:32 -0300

    Final code for lesson 19

--- Section_29/01-wild-oasis/src/features/authentication/SignupForm.jsx
+++ Section_29/01-wild-oasis/src/features/authentication/SignupForm.jsx
@@ -1,32 +1,77 @@
-import Button from "../../ui/Button";
-import Form from "../../ui/Form";
-import FormRow from "../../ui/FormRow";
-import Input from "../../ui/Input";
+import { useForm } from 'react-hook-form';
+
+import Button from '../../ui/Button';
+import Form from '../../ui/Form';
+import FormRow from '../../ui/FormRow';
+import Input from '../../ui/Input';
 
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
+  const { register, formState, getValues, handleSubmit } = useForm();
+  const { errors } = formState;
+
+  function onSubmit(data) {
+    console.log(data);
+  }
+
   return (
-    <Form>
-      <FormRow label="Full name" error={""}>
-        <Input type="text" id="fullName" />
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <FormRow label='Full name' error={errors?.fullName?.message}>
+        <Input
+          type='text'
+          id='fullName'
+          {...register('fullName', { required: 'This field is required' })}
+        />
       </FormRow>
 
-      <FormRow label="Email address" error={""}>
-        <Input type="email" id="email" />
+      <FormRow label='Email address' error={errors?.email?.message}>
+        <Input
+          type='email'
+          id='email'
+          {...register('email', {
+            required: 'This field is required',
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: 'Please provide a valid e-mail address',
+            },
+          })}
+        />
       </FormRow>
 
-      <FormRow label="Password (min 8 characters)" error={""}>
-        <Input type="password" id="password" />
+      <FormRow
+        label='Password (min 8 characters)'
+        error={errors?.password?.message}
+      >
+        <Input
+          type='password'
+          id='password'
+          {...register('password', {
+            required: 'This field is required',
+            minLength: {
+              value: 8,
+              message: `The password should be at least 8 characters`,
+            },
+          })}
+        />
       </FormRow>
 
-      <FormRow label="Repeat password" error={""}>
-        <Input type="password" id="passwordConfirm" />
+      <FormRow label='Repeat password' error={errors?.passwordConfirm?.message}>
+        <Input
+          type='password'
+          id='passwordConfirm'
+          {...register('passwordConfirm', {
+            required: 'This field is required',
+            validate: (value) =>
+              value === getValues().password ||
+              'Both password inputs need to match',
+          })}
+        />
       </FormRow>
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button variation='secondary' type='reset'>
           Cancel
         </Button>
         <Button>Create new user</Button>
--- Section_29/01-wild-oasis/src/ui/MainNav.jsx
+++ Section_29/01-wild-oasis/src/ui/MainNav.jsx
@@ -78,7 +78,7 @@ function MainNav() {
         <li>
           <StyledNavLink to='/users'>
             <HiOutlineUsers />
-            <span>Account</span>
+            <span>Users</span>
           </StyledNavLink>
         </li>
         <li>
