8ed586bbfefa103560e4c599d8bcd646a02a10e7
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-01-27 17:52:03 -0300

    Final code for lesson 30

--- Section_29/01-wild-oasis/src/ui/ErrorFallback.jsx
+++ Section_29/01-wild-oasis/src/ui/ErrorFallback.jsx
@@ -1,4 +1,7 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import Heading from './Heading';
+import GlobalStyles from '../styles/GlobalStyles';
+import Button from './Button';
 
 const StyledErrorFallback = styled.main`
   height: 100vh;
@@ -24,8 +27,27 @@ const Box = styled.div`
   }
 
   & p {
-    font-family: "Sono";
+    font-family: 'Sono';
     margin-bottom: 3.2rem;
     color: var(--color-grey-500);
   }
 `;
+
+function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledErrorFallback>
+        <Box>
+          <Heading as='h1'>Something went wrong</Heading>
+          <p>{error.message}</p>
+          <Button size='large' onClick={resetErrorBoundary}>
+            Try again
+          </Button>
+        </Box>
+      </StyledErrorFallback>
+    </>
+  );
+}
+
+export default ErrorFallback;
