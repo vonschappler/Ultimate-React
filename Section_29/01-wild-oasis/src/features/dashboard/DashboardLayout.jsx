3de2606ffe05406ec441536271ac0754bc7d86be
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-01-27 17:25:24 -0300

    Final code for lesson 29

--- Section_29/01-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ Section_29/01-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -7,6 +7,7 @@ import Stats from './Stats';
 import { useCabins } from '../cabins/useCabins';
 import SalesChart from './SalesChart';
 import DurationChart from './DurationChart';
+import TodayActivity from '../check-in-out/TodayActivity';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -36,6 +37,7 @@ function DashboardLayout() {
         numDays={numDays}
         cabinsCount={cabins.length}
       />
+      <TodayActivity />
       <DurationChart confirmedStays={confimedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
