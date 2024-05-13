7777d85d4483eed6f1c559cce74acf6f4a6cd8d5
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-05-13 11:00:56 -0300

    Update files to the new supabase project

--- Section_30/01-wild-oasis/src/pages/PageNotFound.jsx
+++ Section_30/01-wild-oasis/src/pages/PageNotFound.jsx
@@ -1,7 +1,8 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-import { useMoveBack } from "../hooks/useMoveBack";
-import Heading from "../ui/Heading";
+import { useMoveBack } from '../hooks/useMoveBack';
+import Heading from '../ui/Heading';
+import Button from '../ui/Button';
 
 const StyledPageNotFound = styled.main`
   height: 100vh;
@@ -33,12 +34,12 @@ function PageNotFound() {
   return (
     <StyledPageNotFound>
       <Box>
-        <Heading as="h1">
+        <Heading as='h1'>
           The page you are looking for could not be found 😢
         </Heading>
-        <button onClick={moveBack} size="large">
+        <Button onClick={moveBack} size='large'>
           &larr; Go back
-        </button>
+        </Button>
       </Box>
     </StyledPageNotFound>
   );
--- Section_30/01-wild-oasis/src/services/supabase.js
+++ Section_30/01-wild-oasis/src/services/supabase.js
@@ -1,13 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
-export const supabaseUrl = 'https://cbyspthjndaovuxvmmvn.supabase.co';
+export const supabaseUrl = 'https://dpwzlknyhcimzyoylvdq.supabase.co';
 
 // error testing url
-// const supabaseUrl = 'https://cbyspthjndaovuxvmmvn.supabase.com';
+// export const supabaseUrl = 'https://dpwzlknyhcimzyoylvdq.supabase.com';
 
 // here we are placing the key directly only because this is a public key protected by RLS with READ only permissions
 // this may later be changed to something more secure, hidden by environmental variables
 const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNieXNwdGhqbmRhb3Z1eHZtbXZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU3NzUzODksImV4cCI6MjAyMTM1MTM4OX0.uGjtzepNJgs9-3k0ORsu3vftIoFp5WUdS4Z8MVAmgyc';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwd3psa255aGNpbXp5b3lsdmRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU1OTc5MzksImV4cCI6MjAzMTE3MzkzOX0.yTo_vJhFUO4coLGAXrj-c6zuE_iyfBrjdJTpRQORJzs';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
