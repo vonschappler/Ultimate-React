3377c2565fb5e4d85aaabe490fb3a1fd801e6f44
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-05-13 11:44:02 -0300

    Move supabase project

--- Section_30/01-wild-oasis/src/services/supabase.js
+++ Section_30/01-wild-oasis/src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-export const supabaseUrl = 'https://dpwzlknyhcimzyoylvdq.supabase.co';
+export const supabaseUrl = 'https://phpolhbattkqhvdbwtky.supabase.co';
 
 // error testing url
 // export const supabaseUrl = 'https://dpwzlknyhcimzyoylvdq.supabase.com';
@@ -7,7 +7,7 @@ export const supabaseUrl = 'https://dpwzlknyhcimzyoylvdq.supabase.co';
 // here we are placing the key directly only because this is a public key protected by RLS with READ only permissions
 // this may later be changed to something more secure, hidden by environmental variables
 const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwd3psa255aGNpbXp5b3lsdmRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU1OTc5MzksImV4cCI6MjAzMTE3MzkzOX0.yTo_vJhFUO4coLGAXrj-c6zuE_iyfBrjdJTpRQORJzs';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBocG9saGJhdHRrcWh2ZGJ3dGt5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU2MDk2ODksImV4cCI6MjAzMTE4NTY4OX0.2IM0f1COGafC2V-yT0Fw8eMzpocS4nUdIJUEgY19RTU';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
