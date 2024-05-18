ab87063432e93d5c541470c1922a57000c702a4e
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-05-18 19:35:02 -0300

    Final code for lesson 08

--- Section_32/02-wild-oasis-next/app/cabins/page.js
+++ Section_32/02-wild-oasis-next/app/cabins/page.js
@@ -1,3 +1,5 @@
+import Counter from '../components/Counter';
+
 export default async function Page() {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
   const data = await res.json();
@@ -10,6 +12,7 @@ export default async function Page() {
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
+      <Counter users={data} />
     </div>
   );
 }
