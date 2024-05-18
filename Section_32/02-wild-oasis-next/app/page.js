7d5519a924c93f0583c4455d295afac0fb26639b
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-05-18 15:07:08 -0300

    Final code for lesson 06

--- Section_32/02-wild-oasis-next/app/page.js
+++ Section_32/02-wild-oasis-next/app/page.js
@@ -1,10 +1,6 @@
-import Link from 'next/link';
-import Navigation from './components/Navigation';
-
 export default function Page() {
   return (
     <div>
-      <Navigation />
       <h1>The Wild Oasis. Welcome to paradise.</h1>
     </div>
   );
