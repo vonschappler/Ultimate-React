5538e866027fe3d957831717e2fa26dd4c1f07ab
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-05-19 14:28:48 -0300

    Final code for lesson 03

--- Section_33/01-wild-oasis-next/app/account/page.js
+++ Section_33/01-wild-oasis-next/app/account/page.js
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: 'Guest area',
+};
+
 export default function Page() {
   return <h1>Your Account</h1>;
 }
--- Section_33/01-wild-oasis-next/app/layout.js
+++ Section_33/01-wild-oasis-next/app/layout.js
@@ -4,7 +4,13 @@ import Navigation from '@/app/_components/Navigation';
 import '@/app/_styles/globals.css';
 
 export const metadata = {
-  title: 'The Wild Oasis',
+  title: {
+    template: '%s / The Wild Oasis',
+    default: 'Welcome / The Wild Oasis',
+  },
+  description:
+    'Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests',
+  
 };
 
 export default function RootLayout({ children }) {
