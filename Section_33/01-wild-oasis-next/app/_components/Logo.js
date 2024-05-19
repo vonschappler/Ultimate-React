f2d672605c926f417da9c35e62efa3684fcb9727
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-05-19 15:58:29 -0300

    Final code for lesson 06

--- Section_33/01-wild-oasis-next/app/_components/Logo.js
+++ Section_33/01-wild-oasis-next/app/_components/Logo.js
@@ -1,9 +1,17 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import logo from '@/public/logo.png';
 
 function Logo() {
   return (
     <Link href='/' className='flex items-center gap-4 z-10'>
-      <img src='/logo.png' height='60' width='60' alt='The Wild Oasis logo' />
+      <Image
+        src={logo}
+        height='60'
+        width='60'
+        alt='The Wild Oasis logo'
+        quality={100}
+      />
       <span className='text-xl font-semibold text-primary-100'>
         The Wild Oasis
       </span>
