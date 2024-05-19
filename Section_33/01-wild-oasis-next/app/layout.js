f551bbed7a92c827dc1a5f58731a1e27eb13c3b2
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-05-19 17:07:56 -0300

    Final code for lesson 07

--- Section_33/01-wild-oasis-next/app/layout.js
+++ Section_33/01-wild-oasis-next/app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased`}
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased relative`}
       >
         <Header />
         <div className='flex-1 px-8 py-12'>
--- Section_33/01-wild-oasis-next/app/page.js
+++ Section_33/01-wild-oasis-next/app/page.js
@@ -1,7 +1,31 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import bg from '@/public/bg.png';
+
 export default function Page() {
   return (
-    <div>
-      <h1>The Wild Oasis. Welcome to paradise.</h1>
-    </div>
+    <main className='mt-24'>
+      <Image
+        src={bg}
+        alt='Mountains and forests with two cabins'
+        fill
+        className='object-cover object-top'
+        placeholder='blur'
+        quality={80}
+      />
+
+      <div className='relative z-10 text-center'>
+        <h1 className='text-8xl text-primary-50 mb-10 tracking-tight font-normal'>
+          Welcome to paradise.
+        </h1>
+        <Link
+          href='/cabins'
+          className='bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all'
+        >
+          Explore luxury cabins
+        </Link>
+      </div>
+    </main>
   );
 }
