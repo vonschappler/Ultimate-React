3571ae8ca0afe2fa9ad4196bc0333f948b7ceec4
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-05-19 12:44:47 -0300

    Initial commit

---
+++ Section_33/01-wild-oasis-next/app/cabins/page.js
@@ -0,0 +1,18 @@
+import Counter from '../components/Counter';
+
+export default async function Page() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+  console.log(data);
+  return (
+    <div>
+      <h1>Cabins page</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+      <Counter users={data} />
+    </div>
+  );
+}
---
+++ Section_33/01-wild-oasis-next/app/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>The Wild Oasis. Welcome to paradise.</h1>
+    </div>
+  );
+}
