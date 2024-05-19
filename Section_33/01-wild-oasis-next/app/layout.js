1555f79f1adadbdfe465cc261ab914b1b42d059a
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-05-19 15:12:55 -0300

    Final code for lesson 04

--- Section_33/01-wild-oasis-next/app/layout.js
+++ Section_33/01-wild-oasis-next/app/layout.js
@@ -1,6 +1,13 @@
 import Logo from '@/app/_components/Logo';
 import Navigation from '@/app/_components/Navigation';
 
+import { Josefin_Sans } from 'next/font/google';
+
+const josefin = Josefin_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
 import '@/app/_styles/globals.css';
 
 export const metadata = {
@@ -10,13 +17,14 @@ export const metadata = {
   },
   description:
     'Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests',
-  
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className='bg-primary-950 text-primary-100 min-h-screen'>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
           <Navigation />
