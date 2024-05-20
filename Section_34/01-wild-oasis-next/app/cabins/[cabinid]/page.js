3a10140a2a15ffe02d8d952e2c44d8aeef72d7d8
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-05-20 18:33:38 -0300

    Final code for lesson 06

--- Section_34/01-wild-oasis-next/app/cabins/[cabinid]/page.js
+++ Section_34/01-wild-oasis-next/app/cabins/[cabinid]/page.js
@@ -2,8 +2,15 @@ import Image from 'next/image';
 import { getCabin } from '@/app/_lib/data-service';
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from '@heroicons/react/24/solid';
 
+export async function generateMetadata({ params }) {
+  const { name } = await getCabin(params.cabinId);
+  return {
+    title: `Cabin ${name}`,
+  };
+}
+
 export default async function Page({ params }) {
-  const cabin = await getCabin(params.cabinid);
+  const cabin = await getCabin(params.cabinId);
   const { id, name, maxCapacity, regularPrice, discount, image, description } =
     cabin;
 
