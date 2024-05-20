861608fb1be348af03bf10309a604c469666c2e8
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-05-20 13:24:48 -0300

    Final code for lesson 02

--- Section_34/01-wild-oasis-next/app/_lib/supabase.js
+++ Section_34/01-wild-oasis-next/app/_lib/supabase.js
@@ -3,6 +3,4 @@ import { createClient } from '@supabase/supabase-js';
 const URL = process.env.SUPABASE_URL;
 const KEY = process.env.SUPABASE_KEY;
 
-console.log({ URL, KEY });
-
 export const supabase = createClient(URL, KEY);
