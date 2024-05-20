ab54fec9bfdaedec2f7758683cd09aced696673e
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-05-20 17:41:47 -0300

    Final code for lesson 04

---
+++ Section_34/01-wild-oasis-next/app/_components/CabinList.js
@@ -0,0 +1,16 @@
+import CabinCard from './CabinCard';
+import { getCabins } from '@/app/_lib/data-service';
+
+export default async function CabinList() {
+  const cabins = await getCabins();
+
+  if (!cabins.length) return null;
+
+  return (
+    <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14'>
+      {cabins.map((cabin) => (
+        <CabinCard cabin={cabin} key={cabin.id} />
+      ))}
+    </div>
+  );
+}
--- Section_34/01-wild-oasis-next/app/cabins/page.js
+++ Section_34/01-wild-oasis-next/app/cabins/page.js
@@ -1,14 +1,13 @@
-import CabinCard from '@/app/_components/CabinCard';
-import { getCabins } from '../_lib/data-service';
+import { Suspense } from 'react';
+
+import CabinList from '@/app/_components/CabinList';
+import Spinner from './loading';
 
 export const metadata = {
   title: 'Cabins',
 };
 
-export default async function Page() {
-  // CHANGE
-  const cabins = await getCabins();
-
+export default function Page() {
   return (
     <div>
       <h1 className='text-4xl mb-5 text-accent-400 font-medium'>
@@ -22,14 +21,9 @@ export default async function Page() {
         away from home. The perfect spot for a peaceful, calm vacation. Welcome
         to paradise.
       </p>
-
-      {cabins.length > 0 && (
-        <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14'>
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
