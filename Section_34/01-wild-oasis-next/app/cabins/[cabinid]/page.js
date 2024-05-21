1eeaae7bbf2a6674a6040e46105dc21cfe628728
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-05-21 12:16:48 -0300

    Final code for lesson 10

--- Section_34/01-wild-oasis-next/app/cabins/[cabinid]/page.js
+++ Section_34/01-wild-oasis-next/app/cabins/[cabinid]/page.js
@@ -11,12 +11,9 @@ export async function generateMetadata({ params }) {
 
 export async function generateStaticParams() {
   const cabins = await getCabins();
-  const ids = cabins.map((cabin) => {
-    {
-      cabinId: String(cabin.id);
-    }
-  });
-
+  const ids = cabins.map((cabin) => ({
+    cabinId: String(cabin.id),
+  }));
   return ids;
 }
 
