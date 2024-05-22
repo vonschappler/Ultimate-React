0170e01fc58d9caab2506072e4a1076bfabc939a
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-05-22 18:21:46 -0300

    Final code for lesson 02

--- Section_35/01-wild-oasis-next/app/_components/SideNavigation.js
+++ Section_35/01-wild-oasis-next/app/_components/SideNavigation.js
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import {
   CalendarDaysIcon,
@@ -5,6 +7,7 @@ import {
   UserIcon,
 } from "@heroicons/react/24/solid";
 import SignOutButton from "./SignOutButton";
+import { usePathname } from "next/navigation";
 
 const navLinks = [
   {
@@ -25,13 +28,15 @@ const navLinks = [
 ];
 
 function SideNavigation() {
+  const pathname = usePathname();
+
   return (
     <nav className="border-r border-primary-900">
       <ul className="flex h-full flex-col gap-2 text-lg">
         {navLinks.map((link) => (
           <li key={link.name}>
             <Link
-              className={`flex items-center gap-4 px-5 py-3 font-semibold text-primary-200 transition-colors hover:bg-primary-900 hover:text-primary-100`}
+              className={`flex items-center gap-4 px-5 py-3 font-semibold text-primary-200 transition-colors hover:bg-primary-900 hover:text-primary-100 ${pathname === link.href ? "bg-primary-900" : ""}`}
               href={link.href}
             >
               {link.icon}
