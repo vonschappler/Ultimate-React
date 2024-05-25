4068f9aa866e6590b7d556d4bc641850e3a6e641
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-05-25 14:26:26 -0300

    Final code for section

--- Section_36/01-wild-oasis-next/app/_lib/auth.js
+++ Section_36/01-wild-oasis-next/app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 const authConfig = {
   providers: [
@@ -11,6 +12,23 @@ const authConfig = {
   callbacks: {
     authorized({ auth, request }) {
       return !!auth?.user;
+    },
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+        console.log(existingGuest);
+
+        if (!existingGuest)
+          await createGuest({ email: user.email, fullName: user.name });
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.gestId = guest.id;
+      return session;
     },
   },
   pages: {
