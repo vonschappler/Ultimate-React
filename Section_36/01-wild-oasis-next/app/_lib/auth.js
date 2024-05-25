23cc7e9531d661745ca1461338ea3a1fb91444e6
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-05-25 13:00:17 -0300

    Final code for lesson 03

--- Section_36/01-wild-oasis-next/app/_lib/auth.js
+++ Section_36/01-wild-oasis-next/app/_lib/auth.js
@@ -8,6 +8,11 @@ const authConfig = {
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
+  callbacks: {
+    authorized({ auth, request }) {
+      return !!auth?.user;
+    },
+  },
 };
 
 export const {
