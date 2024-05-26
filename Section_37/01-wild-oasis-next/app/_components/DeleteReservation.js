10265a726decd49d8b892378992fb2e729db089e
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-05-26 12:35:24 -0300

    Final code for lesson 06

--- Section_37/01-wild-oasis-next/app/_components/DeleteReservation.js
+++ Section_37/01-wild-oasis-next/app/_components/DeleteReservation.js
@@ -2,16 +2,33 @@
 
 import { TrashIcon } from "@heroicons/react/24/solid";
 import { deleteResevation } from "../_lib/actions";
+import { useTransition } from "react";
+import Spinner from "./Spinner";
+import SpinnerMini from "./SpinnerMini";
 
 function DeleteReservation({ bookingId }) {
-  console.log(bookingId);
+  const [isPending, startTransition] = useTransition();
+
+  function handleDelete() {
+    if (confirm("Are you sure you want to delete this reservation?"))
+      startTransition(() => deleteResevation(bookingId));
+  }
   return (
     <button
       className="group flex flex-grow items-center gap-2 px-3 text-xs font-bold uppercase text-primary-300 transition-colors hover:bg-accent-600 hover:text-primary-900"
-      onClick={() => deleteResevation(bookingId)}
+      onClick={handleDelete}
+      disabled={isPending}
     >
-      <TrashIcon className="h-5 w-5 text-primary-600 transition-colors group-hover:text-primary-800" />
-      <span className="mt-1">Delete</span>
+      {isPending ? (
+        <span className="mx-auto">
+          <SpinnerMini />
+        </span>
+      ) : (
+        <>
+          <TrashIcon className="h-5 w-5 text-primary-600 transition-colors group-hover:text-primary-800" />
+          <span className="mt-1">Delete</span>
+        </>
+      )}
     </button>
   );
 }
