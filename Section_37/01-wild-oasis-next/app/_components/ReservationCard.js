6817f0b4ae9cb656b6d83707ed0ccaae7510652f
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-05-26 11:37:49 -0300

    Final code for lesson 04

--- Section_37/01-wild-oasis-next/app/_components/ReservationCard.js
+++ Section_37/01-wild-oasis-next/app/_components/ReservationCard.js
@@ -2,6 +2,8 @@ import { format, formatDistance, isPast, isToday, parseISO } from "date-fns";
 import { PencilSquareIcon } from "@heroicons/react/24/solid";
 
 import DeleteReservation from "./DeleteReservation";
+import Image from "next/image";
+import Link from "next/link";
 
 export const formatDistanceFromNow = (dateStr) =>
   formatDistance(parseISO(dateStr), new Date(), {
@@ -25,8 +27,9 @@ function ReservationCard({ booking }) {
   return (
     <div className="flex border border-primary-800">
       <div className="relative aspect-square h-32">
-        <img
+        <Image
           src={image}
+          fill
           alt={`Cabin ${name}`}
           className="border-r border-primary-800 object-cover"
         />
@@ -68,16 +71,18 @@ function ReservationCard({ booking }) {
         </div>
       </div>
 
-      <div className="flex w-[100px] flex-col border-l border-primary-800">
-        <a
-          href={`/account/reservations/edit/${id}`}
-          className="group flex flex-grow items-center gap-2 border-b border-primary-800 px-3 text-xs font-bold uppercase text-primary-300 transition-colors hover:bg-accent-600 hover:text-primary-900"
-        >
-          <PencilSquareIcon className="h-5 w-5 text-primary-600 transition-colors group-hover:text-primary-800" />
-          <span className="mt-1">Edit</span>
-        </a>
-        <DeleteReservation bookingId={id} />
-      </div>
+      {!isPast(startDate) ? (
+        <div className="flex w-[100px] flex-col border-l border-primary-800">
+          <Link
+            href={`/account/reservations/edit/${id}`}
+            className="group flex flex-grow items-center gap-2 border-b border-primary-800 px-3 text-xs font-bold uppercase text-primary-300 transition-colors hover:bg-accent-600 hover:text-primary-900"
+          >
+            <PencilSquareIcon className="h-5 w-5 text-primary-600 transition-colors group-hover:text-primary-800" />
+            <span className="mt-1">Edit</span>
+          </Link>
+          <DeleteReservation bookingId={id} />
+        </div>
+      ) : null}
     </div>
   );
 }
--- Section_37/01-wild-oasis-next/app/account/reservations/page.js
+++ Section_37/01-wild-oasis-next/app/account/reservations/page.js
@@ -1,28 +1,30 @@
-import ReservationCard from '@/app/_components/ReservationCard';
+import ReservationCard from "@/app/_components/ReservationCard";
+import { auth } from "@/app/_lib/auth";
+import { getBookings } from "@/app/_lib/data-service";
 
 export const metadata = {
-  title: 'Reservations',
+  title: "Reservations",
 };
 
-export default function Page() {
-  // CHANGE
-  const bookings = [];
+export default async function Page() {
+  const session = await auth();
+  const bookings = await getBookings(session.user.guestId);
 
   return (
     <div>
-      <h2 className='font-semibold text-2xl text-accent-400 mb-7'>
+      <h2 className="mb-7 text-2xl font-semibold text-accent-400">
         Your reservations
       </h2>
 
       {bookings.length === 0 ? (
-        <p className='text-lg'>
-          You have no reservations yet. Check out our{' '}
-          <a className='underline text-accent-500' href='/cabins'>
+        <p className="text-lg">
+          You have no reservations yet. Check out our{" "}
+          <a className="text-accent-500 underline" href="/cabins">
             luxury cabins &rarr;
           </a>
         </p>
       ) : (
-        <ul className='space-y-6'>
+        <ul className="space-y-6">
           {bookings.map((booking) => (
             <ReservationCard booking={booking} key={booking.id} />
           ))}
