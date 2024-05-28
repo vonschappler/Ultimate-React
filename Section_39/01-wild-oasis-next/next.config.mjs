a0a3eabc7325b0af6e6217af86ec0a12930c5da6
von Schappler (Manoel Viturino) <vonschappler_dev@outlook.com>
2024-05-28 00:10:33 -0300

    Final code for lesson 01

--- Section_39/01-wild-oasis-next/next.config.mjs
+++ Section_39/01-wild-oasis-next/next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'phpolhbattkqhvdbwtky.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/cabin-images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  // output: 'export',
 };
 
 export default nextConfig;
